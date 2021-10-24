f058e5f94fbd6978e69d8e9eda51c7e81caa645c
Rowan DeLong <rowanwinzer@gmail.com>
2021-10-24 15:08:17 -0600

    Add get one item endpoint and tests

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -6,7 +6,12 @@ class Api::V1::ItemsController < ApplicationController
   end
 
   def show
-    item = Item.find(params[:id])
-    render json: ItemSerializer.new(item).serializable_hash.to_json
+    if Item.exists?(params[:id])
+      item = Item.find(params[:id])
+      render json: ItemSerializer.new(item).serializable_hash.to_json
+    else
+      item = Item.new
+      render json: ItemSerializer.new(item).serializable_hash.to_json, status: :not_found
+    end
   end
 end
